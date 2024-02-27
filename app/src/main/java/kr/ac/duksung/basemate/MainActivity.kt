b23c460647bb7ecf2e4838a0d18c7a6bc441c5a4
kchaeeun <oceanball38@naver.com>
2024-02-27 23:55:32 +0900

    프로젝트 기본 세팅

--- app/src/main/java/kr/ac/duksung/basemate/MainActivity.kt
+++ app/src/main/java/kr/ac/duksung/basemate/MainActivity.kt
@@ -2,10 +2,15 @@ package kr.ac.duksung.basemate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
     }
+
 }
